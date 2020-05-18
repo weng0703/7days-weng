9ef00a60c1231e29e48fa5ed004af14fae6f6f0e
weng <1079374446@qq.com>
2020-05-18 14:31:06 +0800

    create gee

---
+++ gee-web/day1-http-base/base2/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"example/gee"
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	r := gee.New()
+	r.Get("/", func(w http.ResponseWriter, req *http.Request) {
+		fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	})
+	r.Get("/hello", func(w http.ResponseWriter, req *http.Request) {
+		for k, v := range req.Header {
+			fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+		}
+	})
+	r.Run(":9999")
+}
